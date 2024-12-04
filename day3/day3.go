25b7330a9a6dd17644d10a0f4d109c3e3b119a17
Alexandre Betioli <apbetioli@gmail.com>
2024-12-04 09:21:03 +0100

    Day 3 puzzle 2

--- day3/day3.go
+++ day3/day3.go
@@ -33,6 +33,35 @@ func puzzle1() int {
 	return sum
 }
 
+func puzzle2() int {
+	lines := readInput()
+	sum := 0
+	disabled := false
+	for _, line := range lines {
+		r, _ := regexp.Compile(`mul\([0-9]+,[0-9]+\)|do\(\)|don\'t\(\)`)
+		instructions := r.FindAllString(line, -1)
+		for i := 0; i < len(instructions); i++ {
+			instruction := instructions[i]
+			if instruction == "don't()" {
+				disabled = true
+			} else if instruction == "do()" {
+				disabled = false
+			} else if !disabled {
+				instruction = strings.Replace(instruction, "mul(", "", 1)
+				instruction = strings.Replace(instruction, ")", "", 1)
+				digits := strings.Split(instruction, ",")
+				a, _ := strconv.Atoi(digits[0])
+				b, _ := strconv.Atoi(digits[1])
+				mul := a * b
+				sum += mul
+			}
+		}
+
+	}
+	return sum
+}
+
 func main() {
 	fmt.Println(puzzle1())
+	fmt.Println(puzzle2())
 }
