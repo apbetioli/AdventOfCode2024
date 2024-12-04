6ab5b413d80b864accd9a0ba50c0de86d6e09ab6
Alexandre Betioli <apbetioli@gmail.com>
2024-12-04 10:39:53 +0100

    Day 4 puzzle 2

--- day4/day4.go
+++ day4/day4.go
@@ -52,6 +52,43 @@ func countXmas(matrix [][]string, r int, c int) int {
 	return words
 }
 
+func countX_mas(matrix [][]string, r int, c int) int {
+
+	directions := [4][2]int{
+		{-1, -1}, {-1, 1},
+		{1, -1}, {1, 1},
+	}
+
+	m := 2
+	s := 2
+
+	for _, dir := range directions {
+
+		y := r + dir[0]
+		x := c + dir[1]
+
+		if y < 0 || y >= len(matrix) || x < 0 || x >= len(matrix[0]) {
+			break
+		}
+
+		if matrix[y][x] == "M" {
+			m--
+		} else if matrix[y][x] == "S" {
+			s--
+		}
+	}
+
+	if m != 0 || s != 0 {
+		return 0
+	}
+
+	if matrix[r-1][c-1] == matrix[r+1][c+1] { // MAM and SAS
+		return 0
+	}
+
+	return 1
+}
+
 func puzzle1() int {
 	matrix := readInput()
 	total := 0
@@ -68,7 +105,18 @@ func puzzle1() int {
 }
 
 func puzzle2() int {
-	return 0
+	matrix := readInput()
+	total := 0
+
+	for r := 0; r < len(matrix); r++ {
+		for c := 0; c < len(matrix[r]); c++ {
+			if matrix[r][c] == "A" {
+				total += countX_mas(matrix, r, c)
+			}
+		}
+	}
+
+	return total
 }
 
 func main() {
