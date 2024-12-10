a7fb008d792768ca2c8c1effdde10658ff643954
Alexandre Betioli <apbetioli@gmail.com>
2024-12-10 09:49:10 +0100

    Day 8 Puzzle 2

--- day8/day8.go
+++ day8/day8.go
@@ -30,8 +30,6 @@ func readInput() (map[string][]coordinate, int, int, [][]string) {
 	height := len(lines)
 	width := len(lines[0])
 
-	fmt.Println(antennas)
-
 	return antennas, height, width, matrix
 }
 
@@ -42,13 +40,42 @@ func getAntinodes(p1 coordinate, p2 coordinate) (coordinate, coordinate) {
 	a1 := coordinate{p1.y - distance.y, p1.x - distance.x}
 	a2 := coordinate{p2.y + distance.y, p2.x + distance.x}
 
-	fmt.Println("antinode", p1, p2, a1, a2)
-
 	return a1, a2
 }
 
 func isValidAntinode(a coordinate, height int, width int) bool {
 	return a.y >= 0 && a.y < height && a.x >= 0 && a.x < width
+}
+
+func getHarmonicAntinodes(p1 coordinate, p2 coordinate, height int, width int) []coordinate {
+
+	var antinodes []coordinate
+
+	distance := coordinate{p2.y - p1.y, p2.x - p1.x}
+
+	a := p1
+
+	for {
+		a = coordinate{a.y - distance.y, a.x - distance.x}
+		if isValidAntinode(a, height, width) {
+			antinodes = append(antinodes, a)
+		} else {
+			break
+		}
+	}
+
+	a = p2
+
+	for {
+		a = coordinate{a.y + distance.y, a.x + distance.x}
+		if isValidAntinode(a, height, width) {
+			antinodes = append(antinodes, a)
+		} else {
+			break
+		}
+	}
+
+	return antinodes
 }
 
 func puzzle1() int {
@@ -81,13 +108,36 @@ func puzzle1() int {
 		}
 	}
 
-	utils.Debug(matrix)
-
 	return count
 }
 
 func puzzle2() int {
-	return 0
+	antennas, height, width, matrix := readInput()
+
+	count := 0
+
+	for _, positions := range antennas {
+
+		if len(positions) == 1 {
+			continue
+		}
+
+		count += len(positions)
+
+		for p1 := 0; p1 < len(positions)-1; p1++ {
+			for p2 := p1 + 1; p2 < len(positions); p2++ {
+				antinodes := getHarmonicAntinodes(positions[p1], positions[p2], height, width)
+				for _, a1 := range antinodes {
+					if matrix[a1.y][a1.x] == "." {
+						count++
+						matrix[a1.y][a1.x] = "#"
+					}
+				}
+			}
+		}
+	}
+
+	return count
 }
 
 func main() {
