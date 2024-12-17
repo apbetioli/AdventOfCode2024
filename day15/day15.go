9dd9459cf5382a80ed39c80c0e4c2977db61d1ac
Alexandre Betioli <apbetioli@gmail.com>
2024-12-17 12:19:32 +0100

    Day 15 puzzle 2

--- day15/day15.go
+++ day15/day15.go
@@ -42,7 +42,7 @@ func getBoxes(matrix [][]string) [][2]int {
 	var boxes [][2]int
 	for y := range matrix {
 		for x := range matrix[y] {
-			if matrix[y][x] == "O" {
+			if matrix[y][x] == "O" || matrix[y][x] == "[" {
 				boxes = append(boxes, [2]int{y, x})
 			}
 		}
@@ -64,64 +64,24 @@ func puzzle1() int {
 		x := robot[1]
 
 		if move == ">" {
-			if matrix[y][x+1] == "O" {
-				xi := x + 1
-				for ; matrix[y][xi] == "O"; xi++ {
-				}
-				if matrix[y][xi] == "." {
-					matrix[y][xi] = "O"
-					matrix[y][x+1] = "."
-				}
-			}
-			if matrix[y][x+1] == "." {
+			if moveRight(matrix, y, x+1) {
+				swap(matrix, y, x+1, y, x)
 				robot[1] = x + 1
-				matrix[y][x+1] = "@"
-				matrix[y][x] = "."
 			}
 		} else if move == "<" {
-			if matrix[y][x-1] == "O" {
-				xi := x - 1
-				for ; matrix[y][xi] == "O"; xi-- {
-				}
-				if matrix[y][xi] == "." {
-					matrix[y][xi] = "O"
-					matrix[y][x-1] = "."
-				}
-			}
-			if matrix[y][x-1] == "." {
+			if moveLeft(matrix, y, x-1) {
+				swap(matrix, y, x-1, y, x)
 				robot[1] = x - 1
-				matrix[y][x-1] = "@"
-				matrix[y][x] = "."
 			}
 		} else if move == "^" {
-			if matrix[y-1][x] == "O" {
-				yi := y - 1
-				for ; matrix[yi][x] == "O"; yi-- {
-				}
-				if matrix[yi][x] == "." {
-					matrix[yi][x] = "O"
-					matrix[y-1][x] = "."
-				}
-			}
-			if matrix[y-1][x] == "." {
+			if moveUp(matrix, y-1, x) {
+				swap(matrix, y-1, x, y, x)
 				robot[0] = y - 1
-				matrix[y-1][x] = "@"
-				matrix[y][x] = "."
 			}
 		} else if move == "v" {
-			if matrix[y+1][x] == "O" {
-				yi := y + 1
-				for ; matrix[yi][x] == "O"; yi++ {
-				}
-				if matrix[yi][x] == "." {
-					matrix[yi][x] = "O"
-					matrix[y+1][x] = "."
-				}
-			}
-			if matrix[y+1][x] == "." {
+			if moveDown(matrix, y+1, x) {
+				swap(matrix, y+1, x, y, x)
 				robot[0] = y + 1
-				matrix[y+1][x] = "@"
-				matrix[y][x] = "."
 			}
 		}
 
@@ -130,6 +90,10 @@ func puzzle1() int {
 		// fmt.Println()
 	}
 
+	return calculateGPS(matrix)
+}
+
+func calculateGPS(matrix [][]string) int {
 	boxes := getBoxes(matrix)
 
 	sum := 0
@@ -141,8 +105,176 @@ func puzzle1() int {
 	return sum
 }
 
+func resample(matrix [][]string) [][]string {
+	for y := range matrix {
+		var newLine []string
+		for x := 0; x < len(matrix[y]); x++ {
+			if matrix[y][x] == "#" {
+				newLine = append(newLine, "#", "#")
+			} else if matrix[y][x] == "O" {
+				newLine = append(newLine, "[", "]")
+			} else if matrix[y][x] == "@" {
+				newLine = append(newLine, "@", ".")
+			} else if matrix[y][x] == "." {
+				newLine = append(newLine, ".", ".")
+			}
+		}
+		matrix[y] = newLine
+	}
+	return matrix
+}
+
+func swap(matrix [][]string, y1 int, x1 int, y2 int, x2 int) {
+	temp := matrix[y1][x1]
+	matrix[y1][x1] = matrix[y2][x2]
+	matrix[y2][x2] = temp
+}
+
+func moveRight(matrix [][]string, y int, x int) bool {
+	if matrix[y][x] == "." {
+		return true
+	}
+
+	if matrix[y][x] == "[" || matrix[y][x] == "]" || matrix[y][x] == "O" || matrix[y][x] == "@" {
+		ny := y
+		nx := x + 1
+		if moveRight(matrix, ny, nx) {
+			swap(matrix, ny, nx, y, x)
+			return true
+		}
+	}
+
+	return false
+}
+
+func moveLeft(matrix [][]string, y int, x int) bool {
+	if matrix[y][x] == "." {
+		return true
+	}
+
+	if matrix[y][x] == "[" || matrix[y][x] == "]" || matrix[y][x] == "O" || matrix[y][x] == "@" {
+		ny := y
+		nx := x - 1
+		if moveLeft(matrix, ny, nx) {
+			swap(matrix, ny, nx, y, x)
+			return true
+		}
+	}
+
+	return false
+}
+
+func moveUp(matrix [][]string, y int, x int) bool {
+	if matrix[y][x] == "." {
+		return true
+	}
+
+	ny := y - 1
+	nx := x
+
+	if matrix[y][x] == "O" || matrix[y][x] == "@" {
+		if moveUp(matrix, ny, nx) {
+			swap(matrix, ny, nx, y, x)
+			return true
+		}
+	}
+
+	if matrix[y][x] == "[" {
+		if moveUp(matrix, ny, nx) && moveUp(matrix, ny, nx+1) {
+			swap(matrix, ny, nx, y, x)
+			swap(matrix, ny, nx+1, y, x+1)
+			return true
+		}
+	}
+
+	if matrix[y][x] == "]" {
+		if moveUp(matrix, ny, nx) && moveUp(matrix, ny, nx-1) {
+			swap(matrix, ny, nx, y, x)
+			swap(matrix, ny, nx-1, y, x-1)
+			return true
+		}
+	}
+
+	return false
+}
+
+func moveDown(matrix [][]string, y int, x int) bool {
+	if matrix[y][x] == "." {
+		return true
+	}
+
+	ny := y + 1
+	nx := x
+
+	if matrix[y][x] == "O" || matrix[y][x] == "@" {
+		if moveDown(matrix, ny, nx) {
+			swap(matrix, ny, nx, y, x)
+			return true
+		}
+	}
+
+	if matrix[y][x] == "[" {
+		if moveDown(matrix, ny, nx) && moveDown(matrix, ny, nx+1) {
+			swap(matrix, ny, nx, y, x)
+			swap(matrix, ny, nx+1, y, x+1)
+			return true
+		}
+	}
+
+	if matrix[y][x] == "]" {
+		if moveDown(matrix, ny, nx) && moveDown(matrix, ny, nx-1) {
+			swap(matrix, ny, nx, y, x)
+			swap(matrix, ny, nx-1, y, x-1)
+			return true
+		}
+	}
+
+	return false
+}
+
 func puzzle2() int {
-	return 0
+	matrix, movements := readInput()
+
+	matrix = resample(matrix)
+
+	// fmt.Println("Initial state:")
+	// utils.Debug(matrix)
+	// fmt.Println()
+
+	robot := getRobot(matrix)
+
+	for _, move := range movements {
+		y := robot[0]
+		x := robot[1]
+
+		if move == ">" {
+			if moveRight(matrix, y, x+1) {
+				swap(matrix, y, x+1, y, x)
+				robot[1] = x + 1
+			}
+		} else if move == "<" {
+			if moveLeft(matrix, y, x-1) {
+				swap(matrix, y, x-1, y, x)
+				robot[1] = x - 1
+			}
+		} else if move == "^" {
+			if moveUp(matrix, y-1, x) {
+				swap(matrix, y-1, x, y, x)
+				robot[0] = y - 1
+			}
+		} else if move == "v" {
+			if moveDown(matrix, y+1, x) {
+				swap(matrix, y+1, x, y, x)
+				robot[0] = y + 1
+			}
+		}
+
+		// fmt.Println("Move", move, ":")
+		// utils.Debug(matrix)
+		// fmt.Println()
+	}
+
+	return calculateGPS(matrix)
 }
 
 func main() {
